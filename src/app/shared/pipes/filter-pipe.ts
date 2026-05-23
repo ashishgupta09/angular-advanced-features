7e6a3531e3c32d64140ef4bf6fadbb332ae3ed63
ashishgupta09 <ashishgupta95652@gmail.com>
2026-05-23 16:24:08 +0530

    fixed the issues.

--- src/app/shared/pipes/filter-pipe.ts
+++ src/app/shared/pipes/filter-pipe.ts
@@ -5,11 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], search: string): any[] {
-    if (!search) {
+  transform(items: any[], searchText: string, field: string): any[] {
+    if (!searchText) {
       return items;
     }
 
-    return items.filter((item) => item.toLowerCase().includes(search.toLowerCase()));
+    return items.filter((item) => item[field]?.toLowerCase().includes(searchText.toLowerCase()));
   }
 }
